ed08a3d4e946af0c5c52218904230ef3a29af6dc
rg7155 <rg7155@naver.com>
2023-04-10 23:44:37 +0900

    프로젝트 업로드

---
+++ Source/UNSEENProject/UNSEENProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UNSEENProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNSEENPROJECT_API AUNSEENProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
